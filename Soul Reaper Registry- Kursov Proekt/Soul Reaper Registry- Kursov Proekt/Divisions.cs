1656ba54156e7aedc6000678e594810c1a107e2d
CatWizzard8790 <deniyankova1@gmail.com>
2023-01-29 13:39:17 +0200

    Starting the project
    Co-Authored-By: Klameta <74143760+Klameta@users.noreply.github.com>

---
+++ Soul Reaper Registry- Kursov Proekt/Soul Reaper Registry- Kursov Proekt/Divisions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soul_Reaper_Registry__Kursov_Proekt
+{
+    class Divisions
+    {
+    }
+}
---
+++ Soul Reaper Registry- Kursov Proekt/Soul Reaper Registry- Kursov Proekt/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Soul_Reaper_Registry__Kursov_Proekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
