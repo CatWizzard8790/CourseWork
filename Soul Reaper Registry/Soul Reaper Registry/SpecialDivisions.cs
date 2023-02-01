135887bae32b87d627abbc53bf4d29b357421cfc
Klameta <ivanavalcheva@gmail.com>
2023-02-01 02:15:34 +0200

    SpecialDivisions done

---
+++ Soul Reaper Registry/Soul Reaper Registry/SpecialDivisions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soul_Reaper_Registry
+{
+    public class SpecialDivisions
+    {
+        public int SDId { get; private set; }
+        public string Name { get; private set; }
+        public int LeaderId { get; private set; }
+        public string Description { get; private set; }
+        public int DivisionId { get; private set; }
+    }
+}
