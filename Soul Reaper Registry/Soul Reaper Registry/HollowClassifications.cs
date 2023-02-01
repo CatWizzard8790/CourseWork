d89bb1b25df5055bb08f4fd90b70b7b9c6d6bc46
CatWizzard8790 <deniyankova1@gmail.com>
2023-02-01 16:10:54 +0200

    Hollows done

---
+++ Soul Reaper Registry/Soul Reaper Registry/HollowClassifications.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soul_Reaper_Registry
+{
+    class HollowClassifications
+    {
+        public int HCId { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+    }
+}
---
+++ Soul Reaper Registry/Soul Reaper Registry/Hollows.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soul_Reaper_Registry__Kursov_Proekt
+{
+    class Hollows
+    {
+        public int HId { get; private set; }
+        public string Name { get; private set; }
+        public int ClassId { get; private set; }
+        public bool Alive { get; private set; }
+        public int WeaponPowerId { get; private set; }
+        public string Description { get; private set; }
+    }
+}
