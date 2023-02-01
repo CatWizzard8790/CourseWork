726055b898dbdcc9b409a691031ca80deb8a1736
Klameta <ivanavalcheva@gmail.com>
2023-02-01 02:20:42 +0200

    SRMissions done

---
+++ Soul Reaper Registry/Soul Reaper Registry/SoulReaperMissions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soul_Reaper_Registry
+{
+    public class SoulReapersMission
+    {
+        public int SRId { get; private set; }
+        public int MissionId { get; private set; }
+    }
+}
