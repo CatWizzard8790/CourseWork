59b212399f409ab98c6ce62f2e3d48b0e58f9d8c
CatWizzard8790 <deniyankova1@gmail.com>
2023-02-01 16:25:03 +0200

    Missions completed

---
+++ Soul Reaper Registry/Soul Reaper Registry/Missions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soul_Reaper_Registry
+{
+    class Missions
+    {
+        public int MId { get; private set; }
+        public string Name { get; private set; }
+        public DateTime DateCompleted { get; private set; }
+        public string Description { get; private set; }
+    }
+}
