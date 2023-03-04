57e706c9956854a7b87492efaf2e4263a5c2dccf
CatWizzard8790 <deniyankova1@gmail.com>
2023-03-04 13:52:27 +0200

    crtrcf

--- Soul Reaper Registry/Data/SRRContext.cs
+++ Soul Reaper Registry/Data/SRRContext.cs
@@ -7,6 +7,16 @@ namespace Data
     public class SRRContext : DbContext
     {
         public DbSet<SoulReapers> SoulReapers { get; set; }
+        public DBSet<SoulReapersMissions> SoulReapersMissions { get; set; }
+        public DbSet<SpecialDivisions> SpecialDivisions { get; set; }
+        public DBSet<WeaponPowers> WeaponPowers { get; set; }
+        public DBSet<Divisions> Divisions { get; set; }
+        public DBSet<HollowClassifications> HollowClassifications { get; set; }
+        public DBSet<Hollows> Hollows { get; set; }
+        public DBSet<Missions> Missions { get; set; }
+        public DBSet<MissionsHollows> MissionsHollows { get; set; }
+
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             {
