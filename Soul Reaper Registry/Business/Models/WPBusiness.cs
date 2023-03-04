b344bbe8e3edd3e2100e1724df0717c41a57ca2a
Klameta <ivanavalcheva@gmail.com>
2023-03-04 23:44:47 +0200

    Business really done

--- Soul Reaper Registry/Business/Models/WPBusiness.cs
+++ Soul Reaper Registry/Business/Models/WPBusiness.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Data.Models;
+using Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,60 @@ using System.Threading.Tasks;
 
 namespace Business.Models
 {
-    internal class WPBusiness
+    public class WPBusiness
     {
+        private SRRContext sRRContext;
+
+        public List<WeaponPowers> GetAll()
+        {
+            using (sRRContext = new SRRContext())
+            {
+                return sRRContext.WeaponPowers.ToList();
+            }
+        }
+
+        public WeaponPowers Get(int id)
+        {
+            using (sRRContext = new SRRContext())
+            {
+                return sRRContext.WeaponPowers.Find(id);
+            }
+        }
+
+        public void Add(WeaponPowers product)
+        {
+
+            using (sRRContext = new SRRContext())
+            {
+                sRRContext.WeaponPowers.Add(product);
+                sRRContext.SaveChanges();
+            }
+        }
+
+        public void Update(WeaponPowers product)
+        {
+            using (sRRContext = new SRRContext())
+            {
+                var item = sRRContext.WeaponPowers.Find(product.WPId);
+                if (item != null)
+                {
+                    sRRContext.Entry(item).CurrentValues.SetValues(product);
+                    sRRContext.SaveChanges();
+                }
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (sRRContext = new SRRContext())
+            {
+                var item = sRRContext.WeaponPowers.Find(id);
+                if (item != null)
+                {
+                    sRRContext.WeaponPowers.Remove(item);
+                    sRRContext.SaveChanges();
+                }
+            }
+        }
     }
 }
