4f175e24698b5753b8ef09fdec667a01a7bef68b
Klameta <ivanavalcheva@gmail.com>
2023-03-04 18:18:11 +0200

    Adding classes

---
+++ Soul Reaper Registry/SRRAppConsole/Presentation/Display.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SRRAppConsole.Presentation
+{
+    internal class Display
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
