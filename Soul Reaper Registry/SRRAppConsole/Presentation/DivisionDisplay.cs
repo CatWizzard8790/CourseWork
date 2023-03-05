aef6d0f6ee8bb72adb2e6b0ff50920caeeca6f60
CatWizzard8790 <deniyankova1@gmail.com>
2023-03-05 11:45:06 +0200

    Hollow Classifications Display done

--- Soul Reaper Registry/SRRAppConsole/Presentation/DivisionDisplay.cs
+++ Soul Reaper Registry/SRRAppConsole/Presentation/DivisionDisplay.cs
@@ -1,4 +1,4 @@
-﻿using Business;
+﻿using Business.Models;
 using Data.Models;
 using System;
 using System.Collections.Generic;
--- Soul Reaper Registry/SRRAppConsole/Presentation/HollowClasificationDisplay.cs
+++ Soul Reaper Registry/SRRAppConsole/Presentation/HollowClasificationDisplay.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Business.Models;
+using Data.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +8,105 @@ using System.Threading.Tasks;
 
 namespace SRRAppConsole.Presentation
 {
-    public class HollowClasificationDisplay
+    public class HollowClasificationDisplay : Display
     {
+        HCBusiness hCBusiness = new HCBusiness();
+        public override void ShowMenu()
+        {
+            Console.WriteLine(new string('-', 40));
+            Console.WriteLine(@" Hollow Clasification ");
+            Console.WriteLine(new string('-', 40));
+            Console.WriteLine("1. List all entries");
+            Console.WriteLine("2. Add new entry");
+            Console.WriteLine("3. Update entry");
+            Console.WriteLine("4. Fetch entry by ID");
+            Console.WriteLine("5. Delete entry by ID");
+            Console.WriteLine("6. Exit entry");
+        }
+        public override void ListAll()
+        {
+            Console.WriteLine(new string('-', 40));
+            Console.WriteLine(@" Hollow Clasification ");
+            Console.WriteLine(new string('-', 40));
+            var hcs = hCBusiness.GetAll();
+            foreach (var item in hcs)
+            {
+                Console.WriteLine($"Id: {item.HCId}| Name: {item.Name}| Description: {item.Description}|");
+            }
+        }
+        public override void Add()
+        {
+            string data;
+            HollowClassifications Hollows = new HollowClassifications();
+
+            Console.Write("Name: ");
+            Hollows.Name = Console.ReadLine();            
+
+            Console.Write("Description: ");
+            data = Console.ReadLine();
+            if (EmptyStringChecker(data)) Hollows.Description = data;
+
+            hCBusiness.Add(Hollows);
+            Console.WriteLine("The Hollow Classification has been added!");
+        }
+        public override void Update()
+        {
+            string data;
+            Console.Write("Id: ");
+            int id = int.Parse(Console.ReadLine());
+            HollowClassifications hollow = hCBusiness.Get(id);
+            if (hollow != null)
+            {
+                Console.Write("Name: ");
+                hollow.Name = Console.ReadLine();
+
+                Console.Write("Description: ");
+                data = Console.ReadLine();
+                if (EmptyStringChecker(data)) hollow.Description = data;
+
+                hCBusiness.Update(hollow);
+                Console.WriteLine("The Hollow Classification has been updated!");
+            }
+            else
+            {
+                Console.WriteLine("Hollow Classification not found!");
+            }
+        }
+        public override void Fetch()
+        {
+            Console.Write("Id: ");
+            int id = int.Parse(Console.ReadLine());
+            HollowClassifications hcs = hCBusiness.Get(id);
+            if (hcs != null)
+            {
+                Console.WriteLine(new string('-', 40));
+
+                Console.WriteLine("ID: " + hcs.HCId);
+                Console.WriteLine("Name: " + hcs.Name);
+                Console.WriteLine("Description: " + hcs.Description);
+
+                Console.WriteLine(new string('-', 40));
+            }
+            else
+            {
+                Console.WriteLine("Hollow Classification not found!");
+            }
+        }
+        public override void Delete()
+        {
+            Console.Write("Id: ");
+            int id = int.Parse(Console.ReadLine());
+            HollowClassifications hollow = hCBusiness.Get(id);
+            if (hollow != null)
+            {
+                hCBusiness.Delete(id);
+                Console.WriteLine("The Hollow Classification has been deleted!");
+            }
+            else
+            {
+                Console.WriteLine("Hollow Classification not found!");
+            }
+        }
 
     }
 }
