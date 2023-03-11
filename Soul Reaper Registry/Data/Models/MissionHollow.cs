e2e21a4d3839e5940d54b3950d3657231311cb4e
CatWizzard8790 <deniyankova1@gmail.com>
2023-03-11 10:22:40 +0200

    foreign keys try1

--- Soul Reaper Registry/Data/Models/MissionHollow.cs
+++ Soul Reaper Registry/Data/Models/MissionHollow.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,14 +9,15 @@ using System.Threading.Tasks;
 namespace Data.Models
 {
     /// <summary>
-    /// Intermediate table for Hollows and Missions, allowing the Many to Many relation. It also has information about whether the Hollow is dead or alive
+    /// Intermediate table for Hollows and Missions, allowing the Many to Many relation.
     /// </summary>
     public class MissionHollow
     {
         [Key]
         public int MissionsId { get; set; }
 
+        [ForeignKey("HollowId")]
         public int HollowsId { get; set; }
-        public bool HollowStatus { get; set; }
+        public Hollow Hollow { get; set; }
     }
 }
--- Soul Reaper Registry/Data/Models/MissionSoulReaper.cs
+++ Soul Reaper Registry/Data/Models/MissionSoulReaper.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +15,9 @@ namespace Data.Models
     {
         [Key]
         public int SRId { get; set; }
+
+        [ForeignKey("MissionId")]
         public int MissionId { get; set; }
+        public Mission Mission { get; set; }
     }
 }
